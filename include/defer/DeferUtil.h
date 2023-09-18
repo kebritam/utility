da53eb7e5f0c832c51b4a32393ba6e995a8f6ee6
Amirali Tavakoli <amiralitavakoli80@gmail.com>
2023-09-18 18:06:38 +0330

    move dummy DeferOperatorOverloadingHelper struct into DeferUtil

--- include/defer/DeferUtil.h
+++ include/defer/DeferUtil.h
@@ -3,3 +3,8 @@
 #define CONCAT_HELPER(s1,s2) s1##s2
 #define CONCAT(s1,s2) CONCAT_HELPER(s1,s2)
 #define DEFER_VAR_NAME CONCAT(deferVar, __COUNTER__)
+
+namespace kbt
+{
+	struct DeferOperatorOverloadingHelper { };
+}
